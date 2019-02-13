2c01864a8788e649d2765804b1554c74eb738bc9
Jasmin Mousavi <jasminmousavi@wlan-t-226-135-dhcp.CSUChico.EDU>
2019-02-13 11:35:00 -0800

    Pair programming/first Driver

--- main.cpp
+++ main.cpp
@@ -7,13 +7,24 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  string item;
+  int count = 0;
+  do{ 
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
+    if(input == 'A'){
+      cout<<"Enter Grocery: "<<endl;
+      cin>>item;
+      list[count] = item;
+      count++;
+    }
+  }while(input != 'Q' && count != 4);
+  for(int i =0; i<count; i++){
+    cout<<list[i]<<endl;
+  }
   return 0;
 }
